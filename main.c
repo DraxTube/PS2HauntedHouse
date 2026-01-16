de5869907edad3113b2ee2fae871983ddf1ed682
DraxTube <draxtube01@gmail.com>
2026-01-17 00:06:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <tamtypes.h>
-#include <debug.h>
+#include <kernel.h>
+#include <sifrpc.h>
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
 
-// Questo punta al tuo script Lua convertito in array di byte
 extern unsigned char script_lua[];
 extern unsigned int script_lua_size;
 
 int main(int argc, char *argv[]) {
+    SifInitRpc(0);
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    
-    // Esegue il codice Lua integrato
-    luaL_loadbuffer(L, (const char*)script_lua, script_lua_size, "script.lua");
-    lua_pcall(L, 0, 0, 0);
-    
+
+    // Carica ed esegue lo script che ti ho dato prima
+    if (luaL_loadbuffer(L, (const char*)script_lua, script_lua_size, "script.lua") || lua_pcall(L, 0, 0, 0)) {
+        // Errore in caso di fallimento
+        return 1;
+    }
+
     lua_close(L);
     return 0;
 }
